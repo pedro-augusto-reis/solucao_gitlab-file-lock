6bd75168205bf43b4a8b91eaf0b191964d3d3748
PdroReis <pdroreis@gmail.com>
2018-08-19 21:14:52 -0300

    Update pre-receive.rb

--- pre-receive.rb
+++ pre-receive.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
-  2
-  3 params = gets
-  4 arrayHash = params.split(" ")
-  5 arrayArquivosModificados = `git diff --no-commit-id --name-only #{arrayHash[0]}..#{arrayHash[1]}`.split(" ")
-  6 arrayGitLabFileLock = `git show #{arrayHash[1]}:.gitlabfilelock`.split(" ")
-  7
-  8 if (arrayArquivosModificados & arrayGitLabFileLock).length != 0 then
-  9         puts "////////////////////////////"
- 10         puts "***** GitLab File Lock *****"
- 11         puts "////////////////////////////"
- 12         puts "Nao modificar:"
- 13         puts (arrayArquivosModificados & arrayGitLabFileLock)
- 14         exit 1
- 15 else
- 16         exit 0
- 17 end
+
+params = gets
+arrayHash = params.split(" ")
+arrayArquivosModificados = `git diff --no-commit-id --name-only #{arrayHash[0]}..#{arrayHash[1]}`.split(" ")
+arrayGitLabFileLock = `git show #{arrayHash[1]}:.gitlabfilelock`.split(" ")
+
+if (arrayArquivosModificados & arrayGitLabFileLock).length != 0 then
+  puts "////////////////////////////"
+  puts "***** GitLab File Lock *****"
+  puts "////////////////////////////"
+  puts "Nao modificar:"
+  puts (arrayArquivosModificados & arrayGitLabFileLock)
+  exit 1
+else
+  exit 0
+end
