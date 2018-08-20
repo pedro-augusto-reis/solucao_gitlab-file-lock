01b8b22bc865bfc6bdfe6d535fa078e4a1105353
PdroReis <pdroreis@gmail.com>
2018-08-19 22:56:25 -0300

    Update pre-receive.rb

--- ruby/pre-receive.rb
+++ ruby/pre-receive.rb
@@ -3,14 +3,16 @@
 params = gets
 arrayHash = params.split(" ")
 arrayArquivosModificados = `git diff --no-commit-id --name-only #{arrayHash[0]}..#{arrayHash[1]}`.split(" ")
-arrayGitLabFileLock = `git show #{arrayHash[1]}:.gitlabfilelock`.split(" ")
+arrayGitLabFileLock = `git show #{arrayHash[1]}:.gitlabfilelock 2> /dev/null`.split(" ")
 
-if (arrayArquivosModificados & arrayGitLabFileLock).length != 0 then
-  puts "////////////////////////////"
-  puts "***** GitLab File Lock *****"
-  puts "////////////////////////////"
-  puts "Nao modificar:"
+if ((!arrayGitLabFileLock.nil?) && (arrayGitLabFileLock.length != 0) && ((arrayArquivosModificados & arrayGitLabFileLock).length != 0))
+  puts "******************************************************"
+  puts "****************** GitLab File Lock ******************"
+  puts "1) Arquivo travado pelo ADM do repositorio."
+  puts "2) Para conseguir realizar o PUSH, reverta o(s) \narquivo(s) abaixo informado(s) para a revisao anterior\nao commit que o modificou."
+  puts "------------------------------------------------------"
   puts (arrayArquivosModificados & arrayGitLabFileLock)
+  puts "------------------------------------------------------"
   exit 1
 else
   exit 0
